64a16a0ecf641de511dd9f75d5999bf4d598cea6
ValeriaRabch <rabchenukv@gmail.com>
2024-02-26 13:51:56 +0200

    Add struct Student

--- FileName.cpp
+++ FileName.cpp
@@ -2,7 +2,15 @@
 #include <iostream>
 using namespace std;
 
+struct Student {
+	char surname[20], group[5];
+	int marks[5];
+};
+
 int main() {
+	//завдання 1
+	int size = 5;
+	Student* man = new Student[size];
 
 	return 0;
 }

68a0d58d53998ec5fbc41ddedc4e5eb903c06b0b
ValeriaRabch <rabchenukv@gmail.com>
2024-02-26 13:54:37 +0200

    Add func Print

--- FileName.cpp
+++ FileName.cpp
@@ -7,10 +7,38 @@ struct Student {
 	int marks[5];
 };
 
+void Print(Student* man, int size) {
+	for (int i = 0; i < size; i++) {
+
+		cout << "Surname -" << man[i].surname << endl;
+		cout << "Group -" << man[i].group << endl;
+		for (int j = 0; j < 5; j++) {
+			cout << "Marks -";
+			cout << man[i].marks[j] << endl;
+		}
+	}
+}
+
 int main() {
 	//завдання 1
 	int size = 5;
 	Student* man = new Student[size];
+
+	for (int i = 0; i < size; i++) {
+		cout << "Enter surname "; cin >> man[i].surname;
+		cout << "Enter group "; cin >> man[i].group;
+		for (int j = 0; j < 5; j++) {
+			man[i].marks[j] = rand() % 12 + 1;
+		}
+	}
+
+	int a = 1;
+	while (a != 6) {
+		cout << "Select\n1 - print students\n2 - add student\n3 - delete student\n4 - print excellent  students\n5 - print badly students\n6 - leave"; cin >> a;
+		if (a == 1) {
+			Print(man, size);
+		}
+	}
 
 	return 0;
 }
